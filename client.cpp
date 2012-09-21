962b3da8da40ba06090d54be5ce7bcc552ee5af6
silenthunter <silenthunter@gmail.com>
2012-09-20 23:23:36 -0400

    Client work

--- client.cpp
+++ client.cpp
@@ -1,3 +1,8 @@
+struct workerThreadStruct
+{
+	int loopLimit;
+};
+
 /**
  * @brief Allows for connection to and requesting pages from an HTTP Server
  */
@@ -17,5 +22,28 @@ class client
 	client(int remotePort)
 	{
 		port = remotePort;
+	}
+	
+	/**
+	 * @brief the main thread that will spawn worker threads
+	 * to connect to the HTTP server
+	 * 
+	 * @param threadCount The number of threads to spawn
+	 * @param loopLimit How many times each worker thread will run
+	 */
+	void runWorkerThreads(int threadCount, int loopLimit)
+	{
+	}
+	
+	/**
+	 * @brief The worker thread that will actually connect
+	 *  and write to the HTTP server
+	 * 
+	 * @param dataStruct The data structure that contains loop
+	 * and connection information
+	 */
+	void* workerThread(void* dataStruct)
+	{
+		workerThreadStruct* data = (workerThreadStruct*) dataStruct;
 	}
 };
